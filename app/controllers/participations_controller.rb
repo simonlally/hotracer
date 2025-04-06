0569c153e16de40805bf6587412383a0d855f9e1
Simon Lally <simonlallydev@gmail.com>
2025-04-06 13:55:22 -0400

    handle races having the same user participation multiple times

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -2,9 +2,10 @@ class ParticipationsController < ApplicationController
   def create
     @race = Race.find(params[:race_id]) || Race.find_by(slug: params[:slug])
 
-    @participation = @race.participations.new(user: Current.user)
-    if @participation.save
-      redirect_to @race
-    end
+    @participation = @race.participations.find_or_create_by!(user: Current.user)
+    redirect_to @race
+  rescue ActiveRecord::RecordNotFound, ActiveRecord::RecordInvalid => e
+    # log the error somewhere
+    redirect_to races_path, alert: "Failed to join race, try again!"
   end
 end
